60de78ff99dfb3c3000fe58386274fcaae7a53b1
khalilannbiya <syeichkhalil@gmail.com>
2023-04-30 11:45:31 +0700

    feat: add relation table on Users table with Complaints table

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,4 +68,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+    public function complaint()
+    {
+        return $this->hasMany(Complaint::class);
+    }
 }
