1be1b91e8e80599072d801a93460dda90661a57b
khalilannbiya <syeichkhalil@gmail.com>
2023-05-08 19:06:34 +0700

    feat: change view user profile setting

---
+++ resources/views/components/frontend/modal-guidelines.blade.php
@@ -0,0 +1,69 @@
+<div id="modalGuidelines"
+    class="modal invisible opacity-0 transition-all duration-500 ease-in-out z-20 fixed w-screen h-screen top-0 left-0 flex items-center justify-center">
+    <div class="modal-overlay absolute w-full h-screen bg-gray-900 opacity-50"></div>
+
+    <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+
+        <!-- your modal content -->
+        <div class="modal-content py-4 text-left px-6">
+            <div class="flex justify-between items-center pb-3">
+                <p class="text-xl font-bold">Panduan Pengisian</p>
+                <div class="modal-close cursor-pointer">
+                    <i class="ti ti-x text-2xl font-bold"></i>
+                </div>
+            </div>
+
+            <div class="modal-body h-96 overflow-y-scroll">
+                <section>
+                    <h2 class="text-vermillion font-semibold text-lg">Apa itu Pengaduan ?</h2>
+                    <p class="mt-2 text-sm font-medium leading-relaxed text-davys-grey">
+                        Pengaduan adalah tindakan atau proses melaporkan atau menyampaikan keluhan atau masalah
+                        kepada
+                        pihak yang berwenang atau terkait. Pengaduan biasanya dilakukan oleh masyarakat atau
+                        individu
+                        yang merasa dirugikan atau tidak puas terhadap suatu hal, baik itu terkait dengan pelayanan
+                        publik, produk atau layanan yang diberikan oleh suatu instansi atau perusahaan, maupun
+                        perilaku
+                        dari orang atau pihak tertentu. Tujuan pengaduan adalah untuk meminta tindakan atau
+                        perbaikan
+                        atas masalah yang disampaikan agar dapat diatasi dan tidak merugikan pihak yang terkena
+                        dampak
+                        dari masalah tersebut.</p>
+                </section>
+                <section class="mt-4">
+                    <h2 class="text-vermillion font-semibold text-lg leading-normal">Pastikan hal-hal berikut ini
+                        sebelum melapor :
+                    </h2>
+                    <ol class="list-decimal mt-2 text-sm font-medium leading-relaxed pl-6 text-davys-grey">
+                        <li>Laporan yang Anda berikan memiliki kaitan erat dengan kinerja pemerintah.</li>
+                        <li>Laporan yang Anda berikan wajib didalam wilayah jangkauan Desa Lemahmulya.</li>
+                        <li class="mt-1">Menyampaikan pesan dengan bahasa Indonesia yang benar dan baik.</li>
+                        <li class="mt-1">Bukan merupakan ujaran kebencian, SARA, dan caci maki.</li>
+                        <li class="mt-1">Bukan merupakan laporan yang sudah disampaikan dan masih dalam proses
+                            penanganan.</li>
+                    </ol>
+                </section>
+                <section class="mt-4">
+                    <h2 class="text-vermillion font-semibold text-lg leading-normal">Perhatikan kolom-kolom yang
+                        wajib atau opsional untuk diisi.
+                    </h2>
+                    <h6 class="mt-3 font-semibold text-davys-grey">Kolom Wajib: </h6>
+                    <ol class="list-decimal mt-2 text-sm font-medium leading-relaxed pl-6 text-davys-grey">
+                        <li><span class="text-vermillion font-semibold">Judul Aduan</span>: Merupakan kesimpulan
+                            dari suatu
+                            permasalahan , inti dari suatu laporan yang disampaikan.</li>
+                        <li><span class="mt-1 text-vermillion font-semibold">Kategori Aduan</span>: Kategori yang
+                            sesuai dengan laporan yang diadukan.</li>
+                        <li><span class="mt-1 text-vermillion font-semibold">Isi Aduan</span>: Menceritakan
+                            kronologis kejadian yang ingin dikeluhkan (Lebih detail, akan lebih baik).</li>
+                    </ol>
+                    <h6 class="mt-3 font-semibold text-davys-grey">Kolom Opsional: </h6>
+                    <ol class="list-decimal mt-2 text-sm font-medium leading-relaxed pl-6 text-davys-grey">
+                        <li><span class="mt-1 text-vermillion font-semibold">Bukti Foto</span>: Data dukungan berupa
+                            bukti gambar.</li>
+                    </ol>
+                </section>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,4 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded transition duration-500 ease-in-out
+ring-1 ring-black focus:outline-none focus:ring-2 focus:ring-vermillion focus:border-transparent shadow-sm']) !!}>
