afa738676881447552aa1c4bdb65163a05216cbf
khalilannbiya <syeichkhalil@gmail.com>
2023-05-14 21:08:20 +0700

    fix: fix bug when cancel modal response

--- resources/views/components/admin/modal/modal-response.blade.php
+++ resources/views/components/admin/modal/modal-response.blade.php
@@ -35,7 +35,8 @@
                 <label class="block mt-4 text-sm">
                     <textarea id="response" name="response"
                         class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-textarea focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray"
-                        rows="6" placeholder="Masukkan Respon disini." required>{{ old('response') }}</textarea>
+                        rows="6" placeholder="Masukkan Respon disini."
+                        required>{{ old('response') ? old('response') : $complaint->response }}</textarea>
                     @error('title')
                     <span class="text-xs text-red-600 dark:text-red-400">
                         {{ $message }}
@@ -45,7 +46,7 @@
             </div>
             <footer
                 class="flex flex-col items-center justify-end px-6 py-3 -mx-6 -mb-4 space-y-4 sm:space-y-0 sm:space-x-6 sm:flex-row bg-gray-50 dark:bg-gray-800">
-                <button @click="closeModal"
+                <button @click="closeModal" type="button"
                     class="w-full px-5 py-3 text-sm font-medium leading-5 text-white text-gray-700 transition-colors duration-150 border border-gray-300 rounded-lg dark:text-gray-400 sm:px-4 sm:py-2 sm:w-auto active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:outline-none focus:shadow-outline-gray">
                     Cancel
                 </button>
