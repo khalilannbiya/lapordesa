fab0b5b491a544fe59c98bf4b12f16ec5ac10eab
khalilannbiya <syeichkhalil@gmail.com>
2023-05-22 17:55:38 +0700

    feat: create dashboard for role admin

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -1,19 +1,20 @@
 <!-- Desktop sidebar -->
 <aside class="z-20 flex-shrink-0 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block">
     <div class="py-4 text-gray-500 dark:text-gray-400">
-        <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="{{ route('staff.dashboard.index') }}">
+        <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200"
+            href="{{ auth()->user()->role_id === 2 ? route('staff.dashboard.index') : route('admin.dashboard.index') }}">
             LaporDesa
         </a>
         <ul class="mt-6">
             <li class="relative px-6 py-3">
 
-                @if (Route::current()->getName() == 'staff.dashboard.index')
+                @if (in_array(Route::current()->getName(), ['staff.dashboard.index', 'admin.dashboard.index']))
                 <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
                     aria-hidden="true"></span>
                 @endif
 
-                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 {{ Route::current()->getName() == 'staff.dashboard.index' ? 'text-gray-800 dark:text-gray-100' : '' }}"
-                    href="{{ route('staff.dashboard.index') }}">
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 {{ in_array(Route::current()->getName(), ['staff.dashboard.index', 'admin.dashboard.index']) ? 'text-gray-800 dark:text-gray-100' : '' }}"
+                    href="{{ auth()->user()->role_id === 2 ? route('staff.dashboard.index') : route('admin.dashboard.index') }}">
                     <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round"
                         stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                         <path
